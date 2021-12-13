6fa83aa693b1b57909d48ae5be9ce12267035778
Lari Oncescu <larioncescu@yahoo.com>
2021-12-13 14:06:18 +0000

    select needed elements

--- script.js
+++ script.js
@@ -1,1 +1,8 @@
 'use strict';
+
+const buttons = document.querySelectorAll('.show-modal');
+const modalWindow = document.querySelector('.modal');
+const modalClose = document.querySelector('.close-modal');
+const overlay = document.querySelector('.overlay');
+
+console.log(buttons);
