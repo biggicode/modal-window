8f0d7817677e65e0892275b6da03342fb86aee7c
Lari Oncescu <larioncescu@yahoo.com>
2021-12-13 15:15:56 +0000

    close modal when user clicks overlay

--- script.js
+++ script.js
@@ -5,14 +5,19 @@ const modalWindow = document.querySelector('.modal');
 const modalClose = document.querySelector('.close-modal');
 const overlay = document.querySelector('.overlay');
 
-for (let i = 0; i < buttons.length; i++) {
-  buttons[i].addEventListener('click', () => {
-    modalWindow.classList.remove('hidden');
-    overlay.classList.remove('hidden');
-  });
-}
-
-modalClose.addEventListener('click', () => {
+const closeModal = () => {
   modalWindow.classList.add('hidden');
   overlay.classList.add('hidden');
-});
+};
+
+const openModal = () => {
+  modalWindow.classList.remove('hidden');
+  overlay.classList.remove('hidden');
+};
+
+for (let i = 0; i < buttons.length; i++) {
+  buttons[i].addEventListener('click', openModal);
+}
+
+modalClose.addEventListener('click', closeModal);
+overlay.addEventListener('click', closeModal);
